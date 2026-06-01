8664f066d83706820fb1a4f0cdcdc3813eb0eab0
Dawid Plat <111369843+PlatinuPL@users.noreply.github.com>
2026-06-01 11:28:55 +0200

    Update settings.py

--- CRMsys/settings.py
+++ CRMsys/settings.py
@@ -18,8 +18,6 @@ from CrmModuleOne.services import initialize_gspread
 load_dotenv()
 
 
-
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
